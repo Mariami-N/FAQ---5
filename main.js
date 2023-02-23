b348d1bc9775578c7d4a99c6e25b503ed51cf72f
Mariami-N <avetianimari@gmail.com>
2023-02-23 09:58:13 -0800

    faq 5 cont

---
+++ main.js
@@ -0,0 +1,29 @@
+
+let arrow1=document.querySelector(".arrow1");
+let li2=document.querySelector(".li2");
+let li1=document.querySelector(".li1");
+let hr1=document.querySelector (".hr1");
+let hrli1=document.querySelector(".hrli1");
+
+arrow1.addEventListener("click", function() {
+arrow1.classList.toggle("turn");
+li1.classList.toggle("li11");
+li2.classList.toggle("li22");
+hr1.classList.toggle("hr11");
+hrli1.classList.toggle("hrli11");
+});
+
+// let arrow2=document.querySelector(".arrow2");
+// let li4=document.querySelector(".li4");
+// let li3=document.querySelector(".li3");
+// let hrli3=document.querySelector(".hrli3");
+
+// arrow2.addEventListener("click", function() {
+// arrow2.classList.toggle("turn");
+// li3.classList.toggle("li33");
+// li4.classList.toggle("li44");
+// hr.classList.toggle("hr1");
+// hrli3.classList.toggle("hrli33");
+// });
+
+
