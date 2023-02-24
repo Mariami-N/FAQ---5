2bb6f26af504ab0d589b8fe68a646248c80e5b01
Mariami-N <avetianimari@gmail.com>
2023-02-24 01:24:31 -0800

    faq cont

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let arrow1=document.querySelector(".arrow1");
-let li2=document.querySelector(".li2");
 let li1=document.querySelector(".li1");
+let li2=document.querySelector(".li2");
 let hr1=document.querySelector (".hr1");
 let hrli1=document.querySelector(".hrli1");
 
@@ -13,17 +13,58 @@ hr1.classList.toggle("hr11");
 hrli1.classList.toggle("hrli11");
 });
 
-// let arrow2=document.querySelector(".arrow2");
-// let li4=document.querySelector(".li4");
-// let li3=document.querySelector(".li3");
-// let hrli3=document.querySelector(".hrli3");
+let arrow2=document.querySelector(".arrow2");
+let li4=document.querySelector(".li4");
+let li3=document.querySelector(".li3");
+let hr2=document.querySelector(".hr2");
+let hrli2=document.querySelector(".hrli2");
 
-// arrow2.addEventListener("click", function() {
-// arrow2.classList.toggle("turn");
-// li3.classList.toggle("li33");
-// li4.classList.toggle("li44");
-// hr.classList.toggle("hr1");
-// hrli3.classList.toggle("hrli33");
-// });
+arrow2.addEventListener("click", function() {
+arrow2.classList.toggle("turn");
+li3.classList.toggle("li11");
+li4.classList.toggle("li22");
+hr2.classList.toggle("hr11")
+hrli2.classList.toggle("hrli11");
+});
 
+let arrow3=document.querySelector(".arrow3");
+let li5=document.querySelector(".li5");
+let li6=document.querySelector(".li6");
+let hr3=document.querySelector(".hr3")
+let hrli3=document.querySelector(".hrli3");
 
+arrow3.addEventListener("click", function() {
+arrow3.classList.toggle("turn");
+li5.classList.toggle("li11");
+li6.classList.toggle("li22");
+hr3.classList.toggle("hr11")
+hrli3.classList.toggle("hrli11");
+});
+
+let arrow4=document.querySelector(".arrow4");
+let li7=document.querySelector(".li7");
+let li8=document.querySelector(".li8");
+let hr4=document.querySelector(".hr4");
+let hrli4=document.querySelector(".hrli4");
+
+arrow4.addEventListener("click", function() {
+arrow4.classList.toggle("turn");
+li7.classList.toggle("li11");
+li8.classList.toggle("li22");
+hr4.classList.toggle("hr11")
+hrli4.classList.toggle("hrli11");
+});
+
+let arrow5=document.querySelector(".arrow5");
+let li9=document.querySelector(".li9");
+let li10=document.querySelector(".li10");
+let hr5=document.querySelector(".hr5");
+let hrli5=document.querySelector(".hrli5");
+
+arrow5.addEventListener("click", function() {
+arrow5.classList.toggle("turn");
+li9.classList.toggle("li11");
+li10.classList.toggle("li22");
+hr5.classList.toggle("hr11")
+hrli5.classList.toggle("hrli11");
+});
